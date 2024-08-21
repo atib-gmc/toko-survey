638950023d400a09c25276147a37f3ede3b482a7
atib <95947051+atib-gmc@users.noreply.github.com>
2024-08-21 15:15:02 +0000

    chore:ajust some color vairiable in global.css

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function NavBar() {
     return (
-        <div className='w-full flex justify-between'>
-            <div className="logo">
-                Logo
-            </div>
-            <ul className='flex gap-2'>
+        <div className="w-full flex justify-between ">
+            <div className="logo text-xl font-bold">Maxima Survey.</div>
+            <ul className="flex gap-2">
                 <li>Home</li>
                 <li>Produk</li>
                 <li>Tentang</li>
             </ul>
             <div className="search">
                 <label htmlFor="">
-                    <input type="text" />
+                    <input
+                        type="text"
+                        className="bg-foreground rounded-full "
+                    />
                 </label>
             </div>
         </div>
-    )
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import NavBar from "./components/NavBar/NavBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <section className="hero bg-primary h-screen w-full rounded-3xl p-8">
+        
+      </section>
     </main>
   );
 }
