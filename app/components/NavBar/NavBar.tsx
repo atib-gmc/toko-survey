ddbcfd3157e8f9f4a1e2c54b63f53a8ee50b839d
atiblp <atiblpu@gmail.com>
2025-08-06 17:58:24 +0700

    fix:fix image error, and improve performance

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -5,18 +5,22 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-  <div className="w-full flex text-blue-700 md:px-[20%] fixed bg-white md:justify-between items-start md:items-center justify-start  shadow-md flex-col md:flex-row">
+  <div className="w-full flex text-blue-700 md:px-[20%]  bg-white md:justify-between items-start md:items-center justify-start  shadow-md flex-col md:flex-row">
       {/* <Link href="/" className="logo text-xl text-gray-900 font-bold"></Link> */}
       <ul className="flex gap-6 w-full items-center font-semibold ">
         <li><Link href="/"  className="flex items-center">
         <Image src="/logo.png" alt="Logo Maxima" width={70} height={70} className="inline-block mr-2" />
         <div className="name flex flex-col ">
           <span className="text-lg font-bold italic text-red-500">Bintang Survey</span>
-          <span className=" text-gray-600 text-xs">Survey Instrument&apos;s</span>
+        <span className=" text-gray-600 text-xs">Survey Instrument&apos;s</span>
         </div>
         </Link></li>
       </ul>
-      
+      <ul className="flex w-full gap-6 items-center justify-center my-3 md:my-0  md:flex font-semibold pr-2">
+        <li><Link href="/#tentang" >Tentang</Link></li>
+        <li><Link href="/produk" >Produk</Link></li>
+        <li><Link href="/#footer" >Hubungi</Link></li>
+      </ul>
       {/* <SearchResult /> */}
     </div>);
 }
