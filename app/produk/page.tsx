e2f50a500c8193d464fc9a224c2ec766c8b1b326
atiblp <atiblpu@gmail.com>
2025-08-06 17:38:53 +0700

    fix:small minor bug

--- app/produk/page.tsx
+++ app/produk/page.tsx
@@ -1,28 +1,6 @@
 // app/products/page.tsx
 import ProductCard from "@/components/ui/ProductCard";
-export const products = [
-  {
-    id: 1,
-    name: "Produk Satu",
-    description: "Deskripsi singkat produk satu.",
-    price: "Rp 120.000",
-    image: "https://placehold.co/600x400",
-  },
-  {
-    id: 2,
-    name: "Produk Dua",
-    description: "Deskripsi singkat produk dua.",
-    price: "Rp 95.000",
-    image: "https://placehold.co/600x400",
-  },
-  {
-    id: 3,
-    name: "Produk Tiga",
-    description: "Deskripsi singkat produk tiga.",
-    price: "Rp 150.000",
-    image: "https://placehold.co/600x400",
-  },
-];
+import { products } from "../data/products";
 
 export default function ProductListPage() {
   return (
